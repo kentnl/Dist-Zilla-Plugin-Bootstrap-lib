38d5031d5dc2c409fbe15aeab3b7bec6fef9189b
Kent Fredric <kentfredric@gmail.com>
2013-08-31 11:01:49 +1200

    Build results of bbe2177 (on master)

--- lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
@@ -111,7 +111,7 @@ sub register_component {
 
   my $it = $root->iterator({ recurse => 1 });
   while ( my $file = $it->() ) {
-#      next unless $file->basename =~ /\.pm$/;
+      next unless $file->basename =~ /\.pm$/;
       my $rpath = $file->relative($root)->stringify;
       $logger->log("$rpath");
   }
