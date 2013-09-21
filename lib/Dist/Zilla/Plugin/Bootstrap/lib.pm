4d6193b4a051317a711ea2a0d518d645cf170800
Kent Fredric <kentfredric@gmail.com>
2013-09-22 00:37:27 +1200

    Build results of 455ea97 (on master)

--- lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
@@ -7,7 +7,7 @@ BEGIN {
   $Dist::Zilla::Plugin::Bootstrap::lib::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::Plugin::Bootstrap::lib::VERSION = '0.04000001';
+  $Dist::Zilla::Plugin::Bootstrap::lib::VERSION = '0.2.0';
 }
 ## use critic;
 
@@ -61,7 +61,7 @@ Dist::Zilla::Plugin::Bootstrap::lib - A minimal boot-strapping for Dist::Zilla P
 
 =head1 VERSION
 
-version 0.04000001
+version 0.2.0
 
 =head1 SYNOPSIS
 
