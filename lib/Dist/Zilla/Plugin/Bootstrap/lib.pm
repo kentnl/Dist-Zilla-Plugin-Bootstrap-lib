ca9eebae749cedae80417314f56ce4b3e58cae4d
Kent Fredric <kentfredric@gmail.com>
2013-11-18 20:23:17 +1300

    Build results of 0a0521e (on master)

--- lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
@@ -7,7 +7,7 @@ BEGIN {
   $Dist::Zilla::Plugin::Bootstrap::lib::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::Plugin::Bootstrap::lib::VERSION = '0.04000001';
+  $Dist::Zilla::Plugin::Bootstrap::lib::VERSION = '0.04000002';
 }
 ## use critic;
 
@@ -61,7 +61,7 @@ Dist::Zilla::Plugin::Bootstrap::lib - A minimal boot-strapping for Dist::Zilla P
 
 =head1 VERSION
 
-version 0.04000001
+version 0.04000002
 
 =head1 SYNOPSIS
 
