84b078e1ce859f993df278ca61216e7c53bad503
Kent Fredric <kentfredric@gmail.com>
2014-07-15 19:44:30 +1200

    v1.000002
     [00 Trivial]
     - Misc packaging cleanups
    
     [Dependencies::Stats]
     - Dependencies changed since 1.000001, see misc/*.deps* for details
     - configure: (recommends: ↑1)
     - develop: +3 -3 (suggests: +2 -1)
     - test: (recommends: ↑1)

--- lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
@@ -7,7 +7,7 @@ use utf8;
 package Dist::Zilla::Plugin::Bootstrap::lib;
 ## use critic;
 
-our $VERSION = '1.000002';
+our $VERSION = '1.000003';
 
 # ABSTRACT: A minimal boot-strapping for Dist::Zilla Plug-ins.
 
