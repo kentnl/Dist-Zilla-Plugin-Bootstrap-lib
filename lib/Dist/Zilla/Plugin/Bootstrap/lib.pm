12eaee3816fd424de2888717a4b010cbc5255988
Kent Fredric <kentfredric@gmail.com>
2014-07-15 19:17:05 +1200

    Build results of da41e09 (on master)

--- lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
@@ -6,7 +6,7 @@ use utf8;
 ## no critic ( NamingConventions::Capitalization )
 package Dist::Zilla::Plugin::Bootstrap::lib;
 ## use critic;
-$Dist::Zilla::Plugin::Bootstrap::lib::VERSION = '1.000001';
+$Dist::Zilla::Plugin::Bootstrap::lib::VERSION = '1.000002';
 # ABSTRACT: A minimal boot-strapping for Dist::Zilla Plug-ins.
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
@@ -197,7 +197,7 @@ Dist::Zilla::Plugin::Bootstrap::lib - A minimal boot-strapping for Dist::Zilla P
 
 =head1 VERSION
 
-version 1.000001
+version 1.000002
 
 =head1 SYNOPSIS
 
