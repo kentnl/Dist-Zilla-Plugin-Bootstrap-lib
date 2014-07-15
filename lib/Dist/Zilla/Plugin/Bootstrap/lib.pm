2b7962d18270607183dcbcd9649a8c4d397cb3e8
Kent Fredric <kentfredric@gmail.com>
2014-07-16 01:19:47 +1200

    v1.000003
     [00 Trivial]
     - use newer Role::Bootstrap ( closes #1 )
    
     [Dependencies::Stats]
     - Dependencies changed since 1.000002, see misc/*.deps* for details
     - develop: (suggests: ↑1)
     - runtime: ↑1

--- lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
@@ -7,7 +7,7 @@ use utf8;
 package Dist::Zilla::Plugin::Bootstrap::lib;
 ## use critic;
 
-our $VERSION = '1.000003';
+our $VERSION = '1.000004';
 
 # ABSTRACT: A minimal boot-strapping for Dist::Zilla Plug-ins.
 
