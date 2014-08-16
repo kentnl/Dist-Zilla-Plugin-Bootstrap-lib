e9c613b22d40e18bae68b19cf72afcd6d7dad62b
Kent Fredric <kentfredric@gmail.com>
2014-08-16 12:53:23 +1200

    v1.000005
     [00 Trivial]
     - CPANDAY!
     - no code changes.
     - whitespace reduction.
    
     [Dependencies::Stats]
     - Dependencies changed since 1.000004, see misc/*.deps* for details
     - develop: +1 (suggests: ↑1)
    
     [Misc]
     - Improve source side POD to reduce build side whitespace.

--- lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/lib.pm
@@ -7,7 +7,7 @@ use utf8;
 package Dist::Zilla::Plugin::Bootstrap::lib;
 ## use critic;
 
-our $VERSION = '1.000005';
+our $VERSION = '1.000006';
 
 # ABSTRACT: A minimal boot-strapping for Dist::Zilla Plug-ins.
 
